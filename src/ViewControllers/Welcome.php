e7ce938cbdb87920da6e1a532c934e7c72ba448f
Dante <dante.palacios@holatrabajo.com>
2023-10-06 12:16:50 -0600

    Update Welcome.php

--- src/ViewControllers/Welcome.php
+++ src/ViewControllers/Welcome.php
@@ -6,6 +6,7 @@ use App\Delegate;
 use App\Model\Model;
 use App\Model\Project;
 use Exception;
+use Locale;
 use Sabatier\Foundation\ArrayClass;
 use Sabatier\Foundation\Bundle;
 use Sabatier\Foundation\Dictionary;
@@ -15,6 +16,7 @@ use Sabatier\Foundation\FileManager;
 use Sabatier\Foundation\Networking\HTTPRequestMethod;
 use Sabatier\Foundation\ObjectClass;
 use Sabatier\Foundation\PropertyListSerialization;
+use Sabatier\Foundation\Set;
 use Sabatier\Foundation\SortDescriptor;
 use Sabatier\Foundation\URL;
 use Sabatier\Foundation\UserDefaults;
@@ -151,9 +153,7 @@ class Welcome extends ViewController
                 kCFBundleShortVersionStringKey => "0.1",
                 kCFBundlePackageTypeKey => "APPL",
                 kCFBundlePrincipalClassKey => Delegate::class,
-                kCFBundleLocalizationsKey => [
-                    "en"
-                ],
+                kCFBundleLocalizationsKey => new Set(["en", Locale::getPrimaryLanguage(Locale::getDefault())]),
                 kCFBundleDocumentTypesKey => [
                     [
                         kCFBundleTypeNameKey => SQLStoreType
