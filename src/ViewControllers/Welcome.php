5f98f75c677a12f9b02f6e26cc19e25b3355dbe3
Dante <dante.palacios@holatrabajo.com>
2023-10-06 12:27:16 -0600

    Update Welcome.php

--- src/ViewControllers/Welcome.php
+++ src/ViewControllers/Welcome.php
@@ -153,7 +153,9 @@ class Welcome extends ViewController
                 kCFBundleShortVersionStringKey => "0.1",
                 kCFBundlePackageTypeKey => "APPL",
                 kCFBundlePrincipalClassKey => Delegate::class,
-                kCFBundleLocalizationsKey => (new Set(["en", Locale::getPrimaryLanguage(Locale::getDefault())]))->toArray(),
+                kCFBundleLocalizationsKey => [
+                    Locale::getPrimaryLanguage(Locale::getDefault())
+                ],
                 kCFBundleDocumentTypesKey => [
                     [
                         kCFBundleTypeNameKey => SQLStoreType
