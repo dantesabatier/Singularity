5e7bb475719e2932b3461c20e61185dcbab34b83
Dante <dante.palacios@holatrabajo.com>
2023-11-27 09:39:03 -0600

    Update LatteRenderer.php

--- src/LatteRenderer.php
+++ src/LatteRenderer.php
@@ -18,7 +18,6 @@ use Sabatier\Foundation\FileManager;
 use Sabatier\Foundation\SearchPathDirectory;
 use Sabatier\Foundation\SearchPathDomainMask;
 use Sabatier\Service\Renderer;
-use function Sabatier\Foundation\class_name;
 use function Sabatier\Foundation\human_readable_value;
 use function Sabatier\Foundation\localized_string;
 use function Sabatier\Foundation\substring_to_index;
@@ -54,7 +53,7 @@ class LatteRenderer extends Renderer
                 }
                 return $fn(AttributeType::undefined);
             }
-            return substring_to_index(class_name($e::class), 1);
+            return substring_to_index($e->entity->name, 1);
         };
         $this->engine->addFunction("img", $img);
         $this->engine->addFunction("localized", fn(string $value): string => localized_string($value));
