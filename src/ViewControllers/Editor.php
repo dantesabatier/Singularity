948da9b17db7d4b8b1cfb8e3414e5ad1865dbafa
Dante <dante.palacios@holatrabajo.com>
2023-11-29 11:15:47 -0600

    Update Editor.php

--- src/ViewControllers/Editor.php
+++ src/ViewControllers/Editor.php
@@ -372,6 +372,8 @@ class Editor extends ViewController
         $project->lastModifiedDate = new Date();
         $model->url = $modelURL;
         $this->managedObjectContext->save();
+        $this->content = json_encode($project->serialized($this->serialization), JSON_PRESERVE_ZERO_FRACTION);
+        $this->contentType = "application/json; charset=utf-8";
     }
 
     /**
@@ -388,6 +390,8 @@ class Editor extends ViewController
             return;
         }
         $model->load($modelURL);
+        $this->content = json_encode($model->serialized($this->serialization), JSON_PRESERVE_ZERO_FRACTION);
+        $this->contentType = "application/json; charset=utf-8";
     }
 
     /**
