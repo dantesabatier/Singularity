c4dc3681a0198d4c5965948e18ce29204155a622
Dante <113949719+dantesabatier@users.noreply.github.com>
2024-01-07 18:06:57 -0600

    Update Editor.php

--- src/ViewControllers/Editor.php
+++ src/ViewControllers/Editor.php
@@ -325,7 +325,6 @@ class Editor extends ViewController
                 "element" => FetchIndexElement::class,
             };
             $fetchRequest = $managedObjectClass::fetchRequest();
-            /** @psalm-suppress InternalClass */
             $fetchRequest->predicate = new ComparisonPredicate(Expression::expressionForKeyPath(SQLEntity::primaryKeyName), Expression::expressionForConstantValue($objectID));
             if (!($selection = $this->managedObjectContext->fetch($fetchRequest)->first())) {
                 break;
