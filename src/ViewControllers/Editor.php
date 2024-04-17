58838e784ea04a9b4d56ef7b5185d2eec08a40e4
Dante <dante.palacios@holatrabajo.com>
2024-04-17 08:08:06 -0600

    Update Editor.php

--- src/ViewControllers/Editor.php
+++ src/ViewControllers/Editor.php
@@ -407,12 +407,11 @@ class Editor extends ViewController
         $namespace = "{$reflectionClass->getNamespaceName()}\\$directory";
         $fileManager = FileManager::default();
         $sourcesURL = $bundle->bundleURL->appendingPathComponent("src");
-        $attributes = new Dictionary([FileAttributeKey::posixPermissions => 0777]);
+        $directoryURL = $sourcesURL->appendingPathComponent($directory);
+        if (!$fileManager->fileExists($directoryURL->path)) {
+            $fileManager->createDirectory($directoryURL, attributes: new Dictionary([FileAttributeKey::posixPermissions => 0777]));
+        }
         foreach ($model->entities as $entity) {
-            $directoryURL = $sourcesURL->appendingPathComponent($directory);
-            if (!$fileManager->fileExists($directoryURL->path)) {
-                $fileManager->createDirectory($directoryURL, attributes: $attributes);
-            }
             $class = $this->class($entity, $namespace);
             $fileURL = $directoryURL->appendingPathComponent($class)->appendPathExtension("php");
             if ($fileManager->createFile($fileURL->path, $this->generateSubclass($entity, $class, $namespace))) {
