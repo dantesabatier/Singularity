d7deeea2242116385ed49d4701fef8cd9be5bd48
dantesabatier <dantesabatier@me.com>
2024-09-13 23:47:44 -0600

    Update Editor.php

--- src/ViewControllers/Editor.php
+++ src/ViewControllers/Editor.php
@@ -223,8 +223,8 @@ class Editor extends ViewController
         $path = $fileURL->path;
         if (FileManager::default()->fileExists($path) && ($contents = FileManager::default()->contents($path)) && ($index = strpos($contents, "class"))) {
             $array = preg_split(sprintf("/%s/", preg_quote("\n", "/")), substring_to_index($contents, $index), -1, PREG_SPLIT_NO_EMPTY);
-            $properties->appendContentsOf(array_filter($array, fn(string $e): bool => str_contains($e, "@property")));
             $uses->appendContentsOf(array_filter($array, fn(string $e): bool => str_starts_with($e, "use")));
+            $properties->appendContentsOf(array_filter($array, fn(string $e): bool => str_contains($e, "@property")));
             $declaration = substring_from_index($contents, $index);
         } else {
             $declaration = "class $class extends ";
