0a8b79cbe0311f5e07b0ac3c75cb994f7669368b
dantesabatier <dantesabatier@me.com>
2024-09-16 09:00:55 -0600

    Update preload.js

--- app/preload.js
+++ app/preload.js
@@ -9,12 +9,13 @@ contextBridge.exposeInMainWorld("api", {
         noLink: true
     }),
     showErrorBox: (error) => ipcRenderer.invoke("showErrorBox", error),
-    showOpenDialog: (title, message, buttonLabel, defaultPath, properties) => ipcRenderer.invoke("showOpenDialog", {
+    showOpenDialog: (title, message, buttonLabel, defaultPath, properties, filters) => ipcRenderer.invoke("showOpenDialog", {
         title: title,
         message: message,
         buttonLabel: buttonLabel,
         defaultPath: defaultPath,
-        properties: properties
+        properties: properties,
+        filters: filters
     }),
     showAboutPanel: (options) => ipcRenderer.send("showAboutPanel", options),
     setProgressBar: (progress => ipcRenderer.send("setProgressBar", progress)),
