7d6349bfbe691530ffe1220e481ef35a9dfebc76
dantesabatier <dantesabatier@me.com>
2024-09-30 04:52:06 -0600

    Update LatteRenderer.php

--- src/LatteRenderer.php
+++ src/LatteRenderer.php
@@ -47,45 +47,57 @@ class LatteRenderer extends Renderer
     public function __get(string $name)
     {
         return $this->$name = match ($name) {
-            "engine" => (function () {
-                $engine = new Engine();
-                $engine->addFilter("readable", fn(mixed $value): string => human_readable_value($value));
-                /** @psalm-suppress ArgumentTypeCoercion */
-                $engine->addFilter("className", fn(string $value): string => class_name($value));
-                $engine->addFilter("camelCase", fn(string $value): string => preg_replace_callback("/\s(.)/", fn(array $matches) => strtoupper($matches[1]), $value));
-                $engine->addFilter("firstLower", fn(string $value): string => lcfirst($value));
-                /** @psalm-suppress InternalMethod */
-                $engine->addFilter("coerced", fn(mixed $value, int $type): mixed => ManagedObject::coercedValue($value, AttributeType::from($type)));
-                $engine->addFilter("nonempty", fn(string $value): ?string => $value === "" ? null : $value);
-                $engine->addFilter("json", fn(mixed $value): string => json_encode($value));
-                $fn = fn(AttributeType $type): string => match ($type) {
-                    AttributeType::integer16, AttributeType::integer32, AttributeType::integer64, AttributeType::decimal, AttributeType::double, AttributeType::float => "N",
-                    AttributeType::uuid, AttributeType::undefined => $type->name,
-                    default => strtoupper(substring_to_index($type->name, 1))
-                };
-                $img = function (Project|Model|Entity|Attribute|Relationship|FetchedProperty|FetchIndex|FetchIndexElement|UniquenessConstraint|FetchRequestTemplate|Configuration $e) use (&$img, &$fn): string {
-                    return match (true) {
-                        $e instanceof Project => "Project",
-                        $e instanceof Model => "Model",
-                        $e instanceof Entity => "E",
-                        $e instanceof Attribute => $fn($e->type),
-                        $e instanceof Relationship => $e->isToMany ? "M" : "O",
-                        $e instanceof FetchedProperty, $e instanceof FetchRequestTemplate => "F",
-                        $e instanceof FetchIndex => "I",
-                        $e instanceof FetchIndexElement => ($property = $e->property) ? $img($property) : $fn(AttributeType::undefined),
-                        $e instanceof UniquenessConstraint => "U",
-                        $e instanceof Configuration => "C",
-                        default => substring_to_index($e->entity->name, 1)
-                    };
-                };
-                $engine->addFunction("img", $img);
-                $engine->addFunction("localized", fn(string $value): string => localized_string($value));
-                $engine->setTempDirectory(FileManager::default()->url(SearchPathDirectory::cachesDirectory, SearchPathDomainMask::local, null, true)->path);
-                $engine->setLoader(new FileLoader($this->bundle->resourceURL?->appendingPathComponent("Views")?->path));
-                return $engine;
-            })(),
+            "engine" => $this->engine(),
             default => throw new Exception("Unknown property $name")
         };
+    }
+
+    private function name(AttributeType $type): string
+    {
+        return match ($type) {
+            AttributeType::integer16, AttributeType::integer32, AttributeType::integer64, AttributeType::decimal, AttributeType::double, AttributeType::float => "N",
+            AttributeType::uuid, AttributeType::undefined => $type->name,
+            default => strtoupper(substring_to_index($type->name, 1))
+        };
+    }
+
+    private function image(Project|Model|Entity|Attribute|Relationship|FetchedProperty|FetchIndex|FetchIndexElement|UniquenessConstraint|FetchRequestTemplate|Configuration $object): string
+    {
+        return match (true) {
+            $object instanceof Project => "Project",
+            $object instanceof Model => "Model",
+            $object instanceof Entity => "E",
+            $object instanceof Attribute => $this->name($object->type),
+            $object instanceof Relationship => $object->isToMany ? "M" : "O",
+            $object instanceof FetchedProperty, $object instanceof FetchRequestTemplate => "F",
+            $object instanceof FetchIndex => "I",
+            $object instanceof FetchIndexElement => ($property = $object->property) ? $this->image($property) : $this->name(AttributeType::undefined),
+            $object instanceof UniquenessConstraint => "U",
+            $object instanceof Configuration => "C",
+            default => substring_to_index($object->entity->name, 1)
+        };
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function engine(): Engine
+    {
+        $engine = new Engine();
+        $engine->addFilter("readable", fn(mixed $value): string => human_readable_value($value));
+        /** @psalm-suppress ArgumentTypeCoercion */
+        $engine->addFilter("className", fn(string $value): string => class_name($value));
+        $engine->addFilter("camelCase", fn(string $value): string => preg_replace_callback("/\s(.)/", fn(array $matches) => strtoupper($matches[1]), $value));
+        $engine->addFilter("firstLower", fn(string $value): string => lcfirst($value));
+        /** @psalm-suppress InternalMethod */
+        $engine->addFilter("coerced", fn(mixed $value, int $type): mixed => ManagedObject::coercedValue($value, AttributeType::from($type)));
+        $engine->addFilter("nonempty", fn(string $value): ?string => $value === "" ? null : $value);
+        $engine->addFilter("json", fn(mixed $value): string => json_encode($value));
+        $engine->addFunction("img", fn(Project|Model|Entity|Attribute|Relationship|FetchedProperty|FetchIndex|FetchIndexElement|UniquenessConstraint|FetchRequestTemplate|Configuration $object): string => $this->image($object));
+        $engine->addFunction("localized", fn(string $value): string => localized_string($value));
+        $engine->setTempDirectory(FileManager::default()->url(SearchPathDirectory::cachesDirectory, SearchPathDomainMask::local, null, true)->path);
+        $engine->setLoader(new FileLoader($this->bundle->resourceURL?->appendingPathComponent("Views")?->path));
+        return $engine;
     }
 
     #[Override]
