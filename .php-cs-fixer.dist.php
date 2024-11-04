d76212814408c016ed37280c2fe8d7976a2d7731
dantesabatier <dantesabatier@me.com>
2024-11-04 09:33:11 -0600

    Create .php-cs-fixer.dist.php

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,14 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())->in(__DIR__);
+
+/** @psalm-suppress InvalidArgument */
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        "@PSR12" => true,
+        "strict_param" => true,
+        "array_syntax" => ["syntax" => "short"],
+        "blank_line_between_import_groups" => false,
+        "closure_fn_spacing" => "none",
+    ])->setFinder($finder);
