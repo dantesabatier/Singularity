5f04b801a6e805efdd693843f7aa1dcea5cf00c2
dantesabatier <dantesabatier@me.com>
2025-05-01 16:11:34 -0600

    Update Viewer.php

--- src/ViewControllers/Viewer.php
+++ src/ViewControllers/Viewer.php
@@ -6,36 +6,68 @@ use App\Model\Project;
 use Sabatier\CoreData\AttributeType;
 use Sabatier\Foundation\ArrayClass;
 use Sabatier\Foundation\Dictionary;
+use Sabatier\Foundation\Networking\HTTPRequestMethod;
 use Sabatier\Foundation\Predicates\Predicate;
 use Sabatier\Foundation\URLComponents;
 use Sabatier\Foundation\URLQueryItem;
+use Sabatier\Foundation\UserDefaults;
+use Sabatier\Service\Action;
 use Sabatier\Service\Endpoint;
 use Sabatier\Service\NotFoundException;
 use Sabatier\Service\Outlet;
 use Sabatier\Service\ViewController;
+use const App\SQLByEntityPositionsMappingTablePreferencesKey;
 
 #[Endpoint]
 class Viewer extends ViewController
 {
     #[Outlet]
-    public Dictionary $schema {
+    public Project $project {
         get {
-            if (!($referenceObject = new URLComponents($this->request->url->absoluteString)->queryItems?->first(fn(URLQueryItem $queryItem): bool => $queryItem->name === "project")?->value)) {
-                throw new NotFoundException();
+            if (!isset($this->project)) {
+                if (!($referenceObject = $this->referenceObject("project"))) {
+                    throw new NotFoundException();
+                }
+                $fetchRequest = Project::fetchRequest();
+                $fetchRequest->predicate = Predicate::format("%K == %s", new ArrayClass(["objectID", $referenceObject]));
+                $fetchRequest->serialization = Dictionary::dictionaryWithArray([
+                    "name" => AttributeType::string,
+                    "url" => AttributeType::uri,
+                    "color" => AttributeType::string
+                ]);
+                $this->project = $this->managedObjectContext->fetch($fetchRequest)->first ?? throw new NotFoundException();
             }
-            $fetchRequest = Project::fetchRequest();
-            $fetchRequest->predicate = Predicate::format("%K == %s", new ArrayClass(["objectID", $referenceObject]));
-            $fetchRequest->serialization = Dictionary::dictionaryWithArray([
-                "name" => AttributeType::string,
-                "url" => AttributeType::uri,
-                "color" => AttributeType::string,
-                "model" => [
-                    "url" => AttributeType::uri
-                ]
-            ]);
-            /** @var Project $project */
-            $project = $this->managedObjectContext->fetch($fetchRequest)->first ?? throw new NotFoundException();
-            return $project->model?->schema;
+            return $this->project;
         }
+    }
+
+    private function referenceObject(string $key): ?int
+    {
+        $referenceObject = $this->request->httpMethod === HTTPRequestMethod::get ? new URLComponents($this->request->url->absoluteString)->queryItems?->first(fn(URLQueryItem $queryItem): bool => $queryItem->name === $key)?->value : $this->request->parsedBody[$key] ?? null;
+        if (is_numeric($referenceObject)) {
+            return (int)$referenceObject;
+        }
+        return null;
+    }
+
+    #[Outlet]
+    public Dictionary $schema {
+        get => $this->project->model?->schema;
+    }
+
+    #[Action]
+    public function moved(): void
+    {
+        $name = $this->project->name;
+        $body = $this->request->parsedBody;
+        /** @var Dictionary<mixed> $dictionary */
+        $dictionary = UserDefaults::standard()->dictionary(SQLByEntityPositionsMappingTablePreferencesKey) ?? new Dictionary();
+        /** @var Dictionary<mixed> $dictionary */
+        $project = $dictionary[$name] ?? new Dictionary();
+        $project[$body["name"]] = $body["pos"];
+        $dictionary[$name] = $project;
+        UserDefaults::standard()->setObject($dictionary, SQLByEntityPositionsMappingTablePreferencesKey);
+        $this->headerFields["Content-Type"] = "application/json";
+        $this->content = json_encode([]);
     }
 }
