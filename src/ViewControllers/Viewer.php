38b330df5b682ac3eaa7a3fb2621a29ff805011e
dantesabatier <dantesabatier@me.com>
2025-05-16 09:09:36 -0600

    Update Viewer.php

--- src/ViewControllers/Viewer.php
+++ src/ViewControllers/Viewer.php
@@ -1,59 +1,20 @@
 <?php
 
-/** @noinspection PhpInternalEntityUsedInspection */
-
 namespace App\ViewControllers;
 
-use App\Model\Project;
-use Sabatier\CoreData\AttributeType;
-use Sabatier\Foundation\ArrayClass;
 use Sabatier\Foundation\Dictionary;
-use Sabatier\Foundation\Networking\HTTPRequestMethod;
-use Sabatier\Foundation\Predicates\Predicate;
-use Sabatier\Foundation\URLComponents;
-use Sabatier\Foundation\URLQueryItem;
 use Sabatier\Foundation\UserDefaults;
 use Sabatier\Service\Action;
 use Sabatier\Service\Endpoint;
-use Sabatier\Service\NotFoundException;
 use Sabatier\Service\Outlet;
-use Sabatier\Service\ViewController;
 use const App\SQLByEntityPositionsMappingTablePreferencesKey;
 
 #[Endpoint]
-class Viewer extends ViewController
+class Viewer extends ProjectViewController
 {
-    #[Outlet]
-    public Project $project {
-        get {
-            if (!isset($this->project)) {
-                if (!($referenceObject = $this->referenceObject("project"))) {
-                    throw new NotFoundException();
-                }
-                $fetchRequest = Project::fetchRequest();
-                $fetchRequest->predicate = Predicate::format("%K == %s", new ArrayClass(["objectID", $referenceObject]));
-                $fetchRequest->serialization = Dictionary::dictionaryWithArray([
-                    "name" => AttributeType::string,
-                    "url" => AttributeType::uri,
-                    "color" => AttributeType::string
-                ]);
-                $this->project = $this->managedObjectContext->fetch($fetchRequest)->first ?? throw new NotFoundException();
-            }
-            return $this->project;
-        }
-    }
     #[Outlet]
     public Dictionary $schema {
         get => $this->project->model?->schema;
-    }
-
-    private function referenceObject(string $key): ?int
-    {
-        $referenceObject = $this->request->httpMethod === HTTPRequestMethod::get ? new URLComponents($this->request->url->absoluteString)->queryItems?->first(fn(URLQueryItem $queryItem): bool => $queryItem->name === $key)?->value : $this->request->parsedBody[$key] ?? null;
-        if (is_numeric($referenceObject)) {
-            return (int)$referenceObject;
-        }
-        return null;
     }
 
     #[Action]
