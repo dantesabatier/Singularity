056b51b583f2d057c3cc531a3bbe39833f7399bd
dantesabatier <dantesabatier@me.com>
2025-05-19 15:33:06 -0600

    Update renderer.js

--- electron/renderer.js
+++ electron/renderer.js
@@ -24,10 +24,6 @@ const url = (endpoint, parameters) => {
  * @param {function|undefined} completion
  */
 const replace = async (url, completion = undefined) => {
-    const e1 = document.getElementById("main")
-    if (!e1) {
-        return
-    }
     const headers = {}
     headers["X-Requested-With"] = "XmlHttpRequest"
     const response = await fetch(url, {
@@ -38,22 +34,14 @@ const replace = async (url, completion = undefined) => {
         await showErrorBox((await response.json())?.error)
         return
     }
-    const data = await response.text()
-    const doc = document.implementation.createHTMLDocument()
-    doc.open()
-    doc.write(data)
-    doc.close()
-    const e2 = doc.getElementById("main")
-    if (!e2) {
-        return
+    const element = document.getElementById("main")
+    if (element) {
+        element.innerHTML = await response.text()
     }
     const info = Array.from(document.querySelectorAll(`[class*="scroll-view"]`)).reduce((obj, e) => {
         obj[e.id] = e.scrollTop
         return obj
     }, {})
-    if (e1.innerHTML !== e2.innerHTML) {
-        e1.innerHTML = e2.innerHTML
-    }
     document.querySelectorAll(`[class*="scroll-view"]`).forEach(e => e.scroll(0, info[e.id]))
     const body = document.body
     body.removeAttribute("class")
