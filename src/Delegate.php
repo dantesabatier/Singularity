31ce043bee25b7b2a1a1d31b96f55e651f42e37b
dantesabatier <dantesabatier@me.com>
2026-01-11 10:27:49 -0600

    Update Delegate.php

--- src/Delegate.php
+++ src/Delegate.php
@@ -24,7 +24,7 @@ final class Delegate extends ObjectClass implements ApplicationDelegate
     #[Override]
     public static function initialize(): void
     {
-        SQLCore::$debugLevel = SQLDebugLevel::prettyFormatSQL;
+        SQLCore::$debugLevel = SQLDebugLevel::none;
         SQLCore::$debugColorOutputDefault = true;
         ViewController::$rendererClass = LatteRenderer::class;
         UserDefaults::standard()->register(new Dictionary([
