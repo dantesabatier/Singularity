beb5ad0f4a2ed2b98b02f8b251e1d58b70b32d95
dantesabatier <dantesabatier@me.com>
2026-03-31 11:27:46 -0600

    Remove unused SQLCore references and clean up Delegate initialization method

--- src/Delegate.php
+++ src/Delegate.php
@@ -1,13 +1,9 @@
 <?php
 
-/** @noinspection PhpInternalEntityUsedInspection */
-
 namespace App;
 
 use Override;
 use Sabatier\CoreData\MergePolicy;
-use Sabatier\CoreData\SQLCore;
-use Sabatier\CoreData\SQLDebugLevel;
 use Sabatier\Foundation\ArrayClass;
 use Sabatier\Foundation\Dictionary;
 use Sabatier\Foundation\ObjectClass;
@@ -26,8 +22,6 @@ final class Delegate extends ObjectClass implements ApplicationDelegate
     #[Override]
     public static function initialize(): void
     {
-        SQLCore::$debugLevel = SQLDebugLevel::none;
-        SQLCore::$debugColorOutputDefault = true;
         ViewController::$rendererClass = LatteRenderer::class;
         UserDefaults::standard()->register(new Dictionary([
             PersistentHistoryTrackingKey => false,
