0352ce508bf2b62f4d170b11c9af250f1f90bfc7
dantesabatier <dantesabatier@me.com>
2026-06-13 10:09:44 -0600

    Add action dispatch logic for "Entity" in EditorController

--- Frontend/ts/Controllers/EditorController.ts
+++ Frontend/ts/Controllers/EditorController.ts
@@ -294,6 +294,11 @@ export class EditorController extends ViewController {
         }
         switch (entity) {
             case "Entity":
+                await this.context.actionDispatcher.dispatch(entity, {
+                    name,
+                    model: parent,
+                })
+                return
             case "FetchRequestTemplate":
             case "Configuration":
             case "CompositeType":
